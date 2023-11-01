c3a2ff493bfed3f2ad17665cf193b6270d151ff6
Kevin Adu-Sarkodie <kevyugi@gmail.com>
2023-11-01 10:01:23 +0900

    Change to netlify deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
-
 import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react()]
+  integrations: [mdx(), react()],
+  output: "server",
+  adapter: netlify()
 });
