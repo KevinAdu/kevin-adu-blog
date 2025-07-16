9e9a8f3c883ba6d0662f98bd1113b908e1e27148
Kevin Adu-Sarkodie <kevyugi@gmail.com>
2025-07-16 10:57:36 +0900

    Fixed the issues with icon and tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
+import icon from "astro-icon";
 
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react()],
+  integrations: [icon(), mdx(), react()],
   adapter: netlify()
 });
